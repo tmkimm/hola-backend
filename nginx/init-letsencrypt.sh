82ea61278c23cca066d5ae6c2bcd16779c226a9e
tmkimm <xoals1417@gmail.com>
2022-01-17 23:24:36 +0900

    feat: Nginx에 SSL 인증서 적용(https)

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(api.holaworld.io)
 rsa_key_size=4096
-data_path="./certbot"
+data_path="./data/certbot"
 email="xoals1417@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
